2223c74e21353e869f88f1ce95f70e2476f44b8c
vaibhavi111220 <158126296+vaibhavi111220@users.noreply.github.com>
2025-05-26 13:53:46 -0400

    Delete blogger directory

--- blogger/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect("localhost","root","","data");
-$delete_id=$_GET['del'];
-$delete_query="delete from details WHERE id='$delete_id'";
-mysqli_query($db,$delete_query);
-header("Location:index.php")
-?>
